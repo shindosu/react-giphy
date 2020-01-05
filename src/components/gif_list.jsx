4a9285a7b905933e78fb944752372a9cefed14cc
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2020-01-05 11:21:09 +0900

    changed fixes

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-import Gif from './gif.jsx';
+import Gif from './gif';
 
+// Destructuring
+// es6 techniques
+// replaced props with separated keys
+// const GifList = ({gifs, selectGif}) => {
+//     return (
+//         <div className="gif-list">
+// can also put id instead of gif, so that you can get rid of gif. makes code shorter
+//             {gifs.map(gif => <Gif id={gif.id} key={gif.id}/>)}
+//         </div>
+//     )
+// }
+
+
+// List Pattern
 class GifList extends Component {
     renderList = () => {
-        return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id}/>);
+      return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
     }
 
     render() {
-        return (
-            <div className="gif-list">
-                {this.renderList()}
-            </div>
-        )
+      return (
+        <div className="gif-list">
+          {this.renderList()}
+        </div>
+      );
     }
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 // import two dependencies
 import React, { Component } from 'react'; // need to write jsx
 import ReactDOM from 'react-dom'; // used at very last line
-import App from './components/app.jsx';
+import App from './components/app.jsx'
 import '../assets/stylesheets/application.scss';
 
 const root = document.getElementById('root'); // get div id
